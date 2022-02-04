ea11a19052078f5ae0294e111120ed207013c6b3
Tadashi <tadash82@gmail.com>
2022-02-04 12:25:31 -0300

    frontend - login

---
+++ frontend/src/services/AuthServices.js
@@ -0,0 +1,9 @@
+export function doLogin(email, password) {
+  if(email=== 'tadash82@gmail.com' && password === '123456') {
+    return true;
+  }
+  return false;
+}
+export function doLogout() {
+
+}

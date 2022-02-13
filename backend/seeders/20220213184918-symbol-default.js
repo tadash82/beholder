59d6fc9b282649cd547f3a979385d8d3fa90511c
Tadashi <tadash82@gmail.com>
2022-02-13 15:57:39 -0300

    tabela symbols

---
+++ backend/seeders/20220213184918-symbol-default.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const symbol = await queryInterface.rawSelect('symbols', { where: {}, limit: 1}, ['symbol']);
+    if(!symbol) {
+      return queryInterface.bulkInsert('symbols', [{
+        symbol: 'BTCBUSD',
+        basePrecision: 8,
+        quotePrecision:8 ,
+        minNotional: '0.1',
+        minLotSize: '0.1',
+        isFavorite: true,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }])
+    }
+  },
+
+  async down (queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('symbols', null, {})
+  }
+};
---
+++ backend/src/models/symbolModel.js
@@ -0,0 +1,35 @@
+const Sequelize = require('sequelize');
+const database = require('../db');
+
+const symbolModel = database.define('symbol', {
+  symbol: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    primaryKey: true
+  },
+  basePrecision: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+  quotePrecision: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+  minNotional: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  minLotSize: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  isFavorite: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+    defaultValue: false
+  },
+  createdAt: Sequelize.DATE,
+  updatedAt: Sequelize.DATE
+})
+
+module.exports = settingsModel;
